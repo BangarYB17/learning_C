129da0fa8eabf3d178d222af2cfd5d3874a0cf2e
BangarYB17 <bangaryogeshwar3@gmail.com>
2026-01-31 11:29:38 +0530

    loops

---
+++ loop_pr.c
@@ -0,0 +1,38 @@
+//print number from 1 to n
+#include<stdio.h>
+int main(){
+//	int i,n;
+//	scanf("%d",&n);
+//    for(i=1;i<=n;i++){ 
+//	printf("%d \n",i);
+
+
+
+//arr[]={10,20,30,40,50,}
+//search element code
+//int arr[]={10,20,30,40,50};
+//int i;
+//int n=5;
+//int num=40;
+//int found=0;
+//
+//  for(i=0;i<n;i++){
+//	if(arr[i]==num){
+//		found=1;
+//		printf("%d",i);
+//		break;
+//	}
+//  }
+
+int i,j;
+for(i=0;i<3;i++){
+//	int a=i;
+	for(j=0;j<5;j++){
+		printf(" %d",i+j);
+//		a++;
+	}
+	printf("\n");
+}
+return 0;
+}
+
